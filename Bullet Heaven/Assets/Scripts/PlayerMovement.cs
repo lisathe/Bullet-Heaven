fe5b831c5205f2b6c77f9705bfa25e2501a874d0
Lisa The <lisathe97@gmail.com>
2018-08-12 21:34:05 +0200

    [Add] Player movement (WASD/Arrow keys) - Add player sprite

---
+++ Bullet Heaven/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    [SerializeField]
+    private float _speed;
+
+	private void Update ()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, vertical, 0);
+        direction = direction * _speed * Time.deltaTime;
+
+        transform.position += direction;
+	}
+}
