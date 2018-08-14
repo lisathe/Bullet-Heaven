5c5f6049e4067c8eefb376dc5203e5a955f851fc
Lisa The <lisathe97@gmail.com>
2018-08-14 11:29:16 +0200

    [Add] Straight Movement

---
+++ Bullet Heaven/Assets/Scripts/EnemyLineMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLineMovement : MonoBehaviour {
+
+    [SerializeField]
+    private float _speed;
+    private Rigidbody2D _rb;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+    void Update ()
+    {
+        _rb.velocity = Vector2.down * _speed;
+
+	}
+}
